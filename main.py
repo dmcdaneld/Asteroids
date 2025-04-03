4b4c2c6f710639570151165b431a984eb47a85e7
dmcdaneld <dylan.mcdaneld@gmail.com>
2025-04-03 05:15:53 -0500

    pygame painting black screen

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_loop = True
+    while game_loop:
+        # check if the user has closed the window and exit the game loop if they do.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0))
+
+
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
